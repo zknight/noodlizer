3b53fdd12d9b604495c746dff273477c012e750d
zayne <zayne.knight@gmail.com>
2025-12-24 14:35:12 -0600

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
-go build
-if [ $? -eq 0 ]; then
-    echo "Build success. Installing at /usr/local/bin and /usr/local/etc/noodlizer"
-    cp noodlizer /usr/local/bin/
-    if [ ! -d "/usr/local/etc/noodlizer" ]; then
-        mkdir /usr/local/etc/noodlizer
-    fi
+cp noodlizer /usr/local/bin/
+if [ ! -d "/usr/local/etc/noodlizer" ]; then
+    echo "noodlizer data dir not exists. Creationing..."
+    mkdir /usr/local/etc/noodlizer
+    chown /usr/local/etc/noodlizer noodlizer
+fi
+cp -r template /usr/local/etc/noodlizer/template/
+cp -r static /usr/local/etc/noodlizer/static/
+if [ ! -a "/usr/local/etc/noodlizer/tracks.db" ]; then
+    echo "noodlizer db don't be. copy it..."
+    cp tracks.db /usr/local/etc/noodlizer/tracks.db
 fi
